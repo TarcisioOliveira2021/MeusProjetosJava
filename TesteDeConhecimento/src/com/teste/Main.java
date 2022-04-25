d024d43b4d99d2021a0473adbed27600014f5d3a
TarcisioOliveira2021 <tarcisio.zark.veloso@gmail.com>
2022-04-25 19:30:08 -0300

    Subindo uma brincadeira melhorada hehehe

--- TesteDeConhecimento/src/com/teste/Main.java
+++ TesteDeConhecimento/src/com/teste/Main.java
@@ -5,11 +5,21 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-	
+		
+	 //Testa se uma string cabe dentro de outra - Método1
 	 List<String> palavra = Arrays.asList("-","x","-");
 	 List<String> palavraEscrita = Arrays.asList("a","x","e");
-	 
-	 
 	 System.out.println(Teste.testarString(palavra,palavraEscrita));
+	 
+	 System.out.println("===============================================");
+	 
+	 //Testa se uma string cabe dentro de outra - Método 2
+	 String[] palavra1 = {"-","-","x"};
+	 String[] palavraEscrita1 = {"b","o","x"};
+	 System.out.println(Teste.testarString1(palavra1,palavraEscrita1));
+	 
+	 //Verifica qual é a maior palavra dentro de um array de palavras.
+	 String[] listaDePalavras = {"apple","beer","sickness"};
+	 System.out.println("A maior palavra é: "+Teste.maiorString(listaDePalavras));
 	}
 }
--- TesteDeConhecimento/src/com/teste/Teste.java
+++ TesteDeConhecimento/src/com/teste/Teste.java
@@ -17,4 +17,33 @@ public class Teste {
 		System.out.println("A palavra: "+palavraEscrita+" cabe dentro de "+palavra);
 		return true;
 	}
+	
+	public static boolean testarString1(String[] palavra1,String[] palavraEscrita1 ) {
+		if (palavra1.length != palavraEscrita1.length) {
+			System.out.println("Não cabe pois o tamanho das duas é diferente");
+			return false;
+		}
+		for (int i = 0; i < palavra1.length; i++) {
+			if (palavra1[i] != "-" && palavra1[i] != palavraEscrita1[i]) {
+				System.out.println("Não cabe pois um elemento não é igual");
+				return false;
+			}
+		}
+		System.out.println("Cabe");
+		return true;
+	}
+	
+	public static String maiorString(String[] lista) {
+		String palavraMaior = "";
+		int aux  = 0;
+		int aux1 = 0;
+		
+		for (int i = 0; i < lista.length; i++) {
+			aux = lista[i].length();
+			if (aux > aux1) {
+				palavraMaior = lista[i];
+			}
+		}
+		return palavraMaior;
+	}
 }
